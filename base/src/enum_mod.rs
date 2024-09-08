4df667f35320ec0038cd7afe60d312788f53b7a5
zdzDesigner <1696498456@qq.com>
2024-09-08 15:43:52 +0800

    ~

---
+++ base/src/enum_mod.rs
@@ -0,0 +1,53 @@
+#[derive(Debug)]
+pub enum Ip {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+pub enum UsState {
+    Alabama,
+    Alaska,
+}
+
+pub enum Coin {
+    Dime,
+    Quarter(UsState),
+}
+
+pub fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+// 二次匹配
+pub fn value_in_cents_two(coin: Coin) -> u8 {
+    match coin {
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            match state {
+                UsState::Alabama => {
+                    println!("{:?}", state);
+                }
+                UsState::Alaska => {
+                    println!("{:?}", state);
+                }
+            }
+            25
+        }
+    }
+}
+
+pub fn equal() {
+    if let Some(3) = Some(3u8) {
+        println!("three");
+    }
+    if let Some(3u8) = Some(3) {
+        println!("three");
+    }
+}
--- base/src/main.rs
+++ base/src/main.rs
@@ -2,13 +2,17 @@
 // mod tuple;
 // mod typebase;
 // mod ownership;
+mod enum_mod;
 mod struct_mod;
+mod module_mod;
 
 fn main() {
     // print::print();
     // tuple::logic();
     // typebase::logic();
     // ownership::ownership();
+
+    // struct_mod ====================================
     let rect = struct_mod::Rectangle {
         width: 20,
         height: 30,
@@ -18,4 +22,16 @@ fn main() {
     println!("rect1 is {:?}", rect);
 
     println!("area:{}", struct_mod::Rectangle::square(10).area());
+
+    // enum_mod ====================================
+
+    println!("{:?}", enum_mod::Ip::V4);
+    println!("{:?}", enum_mod::Ip::V6);
+
+    enum_mod::value_in_cents(enum_mod::Coin::Quarter(enum_mod::UsState::Alabama));
+
+
+    enum_mod::equal();
+
+    module_mod::eat_at_restaurant();
 }
