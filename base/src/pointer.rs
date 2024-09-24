da77d51cd18f7fdf536fd3ba29931c0efdd67501
zdzDesigner <1696498456@qq.com>
2024-09-24 19:07:45 +0800

    ~

--- base/src/pointer.rs
+++ base/src/pointer.rs
@@ -10,6 +10,16 @@ pub fn logic() {
     }
 }
 
+fn addr() {
+    let a = 10;
+    let a_ptr = &a as *const i32; // 获取 a 的内存地址（指针）
+    println!("a 的地址是: {:?}", a_ptr); // 0x7f0572aa04cc
+    println!("a 的地址是: {:p}", &a); // 0x7f0572aa04cc, {:p} 内部转换为 &a as *const i32
+    println!("a 的值: {:?}", &a); // 10
+
+    // 之前说到的 &a 是创建一个引用，而 {:p} 占位符则会显示该引用指向的数据在内存中的地址。
+}
+
 // test ====================
 #[cfg(test)]
 mod tests {
@@ -18,5 +28,6 @@ mod tests {
     #[test]
     fn test_pointer_logic() {
         logic();
+        addr();
     }
 }
