7bdf77e6f4732195441b2030e4d92d98d7daeb3d
zdzDesigner <1696498456@qq.com>
2025-05-07 23:02:48 +0800

    ~

--- base/src/iter.rs
+++ base/src/iter.rs
@@ -30,3 +30,27 @@ mod test_iter {
     #[test]
     fn collect() {}
 }
+
+#[cfg(test)]
+mod test_iter_peekable {
+    #[test]
+    fn iter_peekable() {
+        let expr = "abcd";
+        let iter = expr.chars().peekable();
+        println!("{:?}", iter); // Peekable { iter: Chars(['a', 'b', 'c', 'd']), peeked: None }
+        let v2 = iter.peekable();
+        println!("{:?}", v2); // Peekable { iter: Peekable { iter: Chars(['a', 'b', 'c', 'd']), peeked: None }, peeked: None }
+    }
+    #[test]
+    fn iter_peekable2() {
+        let expr = "abcd";
+        let mut iter = expr.chars().peekable();
+        println!("iter:{:?}", iter);
+        println!("{:?}", iter.peek()); // 存入peeked
+        println!("{:?}", iter.peek());
+        println!("{:?}", iter.next()); // 优先从peeked中取出
+        // println!("{:?}", iter.next());
+        println!("iter:{:?}", iter);
+        println!("expr:{:?}",expr);
+    }
+}
--- base/src/macro_lib/src/lib.rs
+++ base/src/macro_lib/src/lib.rs
@@ -1,33 +1,33 @@
-extern crate proc_macro;
-
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Meta};
-
-#[proc_macro_attribute]
-pub fn my_attribute(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args = parse_macro_input!(args as AttributeArgs);
-    let input = parse_macro_input!(input as ItemFn);
-
-    let arg_str = match &args[0] {
-        NestedMeta::Meta(Meta::NameValue(nv)) => {
-            if let Some(ident) = nv.path.get_ident() {
-                ident.to_string()
-            } else {
-                panic!("Expected named argument");
-            }
-        }
-        _ => panic!("Expected named argument"),
-    };
-
-    let fn_name = &input.sig.ident;
-    let expanded = quote! {
-        println!("Attribute argument: {}", #arg_str);
-        fn #fn_name() {
-            println!("Hello from the decorated function!");
-            #input
-        }
-    };
-
-    TokenStream::from(expanded)
-}
+// extern crate proc_macro;
+//
+// use proc_macro::TokenStream;
+// use quote::quote;
+// use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Meta};
+//
+// #[proc_macro_attribute]
+// pub fn my_attribute(args: TokenStream, input: TokenStream) -> TokenStream {
+//     let args = parse_macro_input!(args as AttributeArgs);
+//     let input = parse_macro_input!(input as ItemFn);
+//
+//     let arg_str = match &args[0] {
+//         NestedMeta::Meta(Meta::NameValue(nv)) => {
+//             if let Some(ident) = nv.path.get_ident() {
+//                 ident.to_string()
+//             } else {
+//                 panic!("Expected named argument");
+//             }
+//         }
+//         _ => panic!("Expected named argument"),
+//     };
+//
+//     let fn_name = &input.sig.ident;
+//     let expanded = quote! {
+//         println!("Attribute argument: {}", #arg_str);
+//         fn #fn_name() {
+//             println!("Hello from the decorated function!");
+//             #input
+//         }
+//     };
+//
+//     TokenStream::from(expanded)
+// }
