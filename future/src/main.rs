a8bfa8e5f5b4b19bb1f6b5836562ee24070e669a
zdzDesigner <1696498456@qq.com>
2025-08-21 19:20:23 +0800

    ~

--- future/src/main.rs
+++ future/src/main.rs
@@ -15,33 +15,4 @@ async fn main() {
     _ = out2.await;
     println!("Hello, world end!");
 
-    // let addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1),8089);
-    // std::net::Ipv4Addr::new(a, b, c, d)
-    // let ip
-    let listener = TcpListener::bind("127.0.0.1:8099").unwrap();
-    // loop {
-    //     match listener.accept() {
-    //         Ok((mut socket, addr)) => {
-    //             println!("new client: {addr:?}");
-    //             let size = socket
-    //                 .write("HTTP/1.1 200 OK\r\n\r\nServer:OK".as_bytes())
-    //                 .unwrap();
-    //             println!("size:{size}");
-    //         }
-    //         Err(e) => println!("couldn't get client: {e:?}"),
-    //     };
-    // }
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let size = stream
-                    .write("HTTP/1.1 200 OK\r\n\r\nServer:OK".as_bytes())
-                    .unwrap();
-
-                stream.flush().unwrap(); // 长连接场景
-                println!("size:{size}");
-            }
-            Err(e) => println!("couldn't get client: {e:?}"),
-        };
-    }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{prelude::*, BufReader},
+    io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
@@ -16,6 +16,38 @@ fn main() {
         thread::spawn(|| {
             handle_connection(stream);
         });
+    }
+}
+
+fn server() {
+    // let addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1),8089);
+    // std::net::Ipv4Addr::new(a, b, c, d)
+    // let ip
+    let listener = TcpListener::bind("127.0.0.1:8099").unwrap();
+    // loop {
+    //     match listener.accept() {
+    //         Ok((mut socket, addr)) => {
+    //             println!("new client: {addr:?}");
+    //             let size = socket
+    //                 .write("HTTP/1.1 200 OK\r\n\r\nServer:OK".as_bytes())
+    //                 .unwrap();
+    //             println!("size:{size}");
+    //         }
+    //         Err(e) => println!("couldn't get client: {e:?}"),
+    //     };
+    // }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let size = stream
+                    .write("HTTP/1.1 200 OK\r\n\r\nServer:OK".as_bytes())
+                    .unwrap();
+
+                stream.flush().unwrap(); // 长连接场景
+                println!("size:{size}");
+            }
+            Err(e) => println!("couldn't get client: {e:?}"),
+        };
     }
 }
 
