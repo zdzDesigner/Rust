428b15fc38b30e493840cf56b032615fc70f7ad1
zdzDesigner <1696498456@qq.com>
2025-09-22 23:27:24 +0800

    ~

--- module/bus/src/lib.rs
+++ module/bus/src/lib.rs
@@ -1,5 +1,8 @@
+pub mod components;
 pub mod math;
 pub mod utils;
+
+pub use components::input::Input;
 
 pub fn getlib() -> String {
     return String::from("xxxx");
--- module/bus/src/main.rs
+++ module/bus/src/main.rs
@@ -1,8 +1,10 @@
 use bus::getlib;
 use bus::utils::tool::get_tool;
+use bus::Input;
 
 fn main() {
     println!("Hello, world!");
     println!("{}", getlib());
     println!("{}", get_tool());
+    println!("{:?}", Input {});
 }
